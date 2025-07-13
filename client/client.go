e36dd63963d27de2086661d64c37f9ddf8cf1e37
upc <upc@europeia.dev>
2025-07-12 20:14:02 -0400

    feat: dispatch functions

--- client/client.go
+++ client/client.go
@@ -13,12 +13,12 @@ import (
 )
 
 type Client struct {
-	username     string
-	password     string
-	token        string
-	base_url     string
-	last_refresh time.Time
-	client       http.Client
+	username    string
+	password    string
+	token       string
+	baseUrl     string
+	lastRefresh time.Time
+	client      http.Client
 }
 
 // creates a new eurocore client.
@@ -28,11 +28,24 @@ func New(username string, password string, base_url string) *Client {
 	client := Client{
 		username: username,
 		password: password,
-		base_url: base_url,
-		client:   http.Client{},
+		baseUrl:  base_url,
+		client: http.Client{
+			Timeout: 5 * time.Second,
+		},
 	}
 
 	return &client
+}
+
+func (c *Client) validateToken() error {
+	if time.Since(c.lastRefresh) > time.Hour {
+		err := c.refreshToken()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (c *Client) refreshToken() error {
@@ -50,7 +63,7 @@ func (c *Client) refreshToken() error {
 		Password: c.password,
 	}
 
-	url := fmt.Sprintf("%s/login", c.base_url)
+	url := fmt.Sprintf("%s/login", c.baseUrl)
 	data, err := json.Marshal(loginData)
 	if err != nil {
 		return err
@@ -75,41 +88,49 @@ func (c *Client) refreshToken() error {
 	}
 
 	c.token = responseData.Token
-	c.last_refresh = time.Now()
+	c.lastRefresh = time.Now()
 
 	return nil
 }
 
-func (c *Client) SendTelegram(t models.Telegram) error {
-	telegrams := []models.Telegram{t}
-
-	return c.SendTelegrams(telegrams)
-}
-
-func (c *Client) SendTelegrams(telegrams []models.Telegram) error {
-	if time.Since(c.last_refresh) > time.Hour {
-		err := c.refreshToken()
-		if err != nil {
-			return err
-		}
+func (c *Client) makeRequest(method string, endpoint string, data []byte) (*http.Response, error) {
+	err := c.validateToken()
+	if err != nil {
+		return nil, err
 	}
 
-	url := fmt.Sprintf("%s/telegrams", c.base_url)
+	url := fmt.Sprintf("%s%s", c.baseUrl, endpoint)
 
-	data, err := json.Marshal(telegrams)
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
 	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	if err != nil {
-		return err
+		return nil, err
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
 
 	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// convenience wrapper around client.SendTelegrams() with a single telegram
+func (c *Client) SendTelegram(t models.NewTelegram) error {
+	telegrams := []models.NewTelegram{t}
+
+	return c.SendTelegrams(telegrams)
+}
+
+func (c *Client) SendTelegrams(t []models.NewTelegram) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.makeRequest("POST", "/telegrams", data)
 	if err != nil {
 		return err
 	}
@@ -120,4 +141,75 @@ func (c *Client) SendTelegrams(telegrams []models.Telegram) error {
 	}
 
 	return nil
+}
+
+func (c *Client) DeleteTelegram(t models.DeleteTelegram) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.makeRequest("DELETE", "/telegrams", data)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		errorText, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("%d: %s", resp.StatusCode, errorText)
+	}
+
+	return nil
+}
+
+func (c *Client) dispatch(method string, endpoint string, data []byte) (models.DispatchStatus, error) {
+	status := models.DispatchStatus{}
+
+	resp, err := c.makeRequest(method, endpoint, data)
+	if err != nil {
+		return status, err
+	}
+
+	if resp.StatusCode != 201 {
+		errorText, _ := io.ReadAll(resp.Body)
+		return status, fmt.Errorf("%d: %s", resp.StatusCode, errorText)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return status, err
+	}
+
+	err = json.Unmarshal(body, &status)
+	if err != nil {
+		return status, err
+	}
+
+	return status, nil
+}
+
+func (c *Client) CreateDispatch(d models.NewDispatch) (models.DispatchStatus, error) {
+	data, err := json.Marshal(d)
+	if err != nil {
+		return models.DispatchStatus{}, err
+	}
+
+	return c.dispatch("POST", "/dispatches", data)
+}
+
+func (c *Client) EditDispatch(d models.EditDispatch) (models.DispatchStatus, error) {
+	data, err := json.Marshal(d)
+	if err != nil {
+		return models.DispatchStatus{}, err
+	}
+
+	endpoint := fmt.Sprintf("/dispatches/%d", d.Id)
+
+	return c.dispatch("PUT", endpoint, data)
+}
+
+func (c *Client) DeleteDispatch(id int) (models.DispatchStatus, error) {
+	endpoint := fmt.Sprintf("/dispatches/%d", id)
+
+	return c.dispatch("PUT", endpoint, []byte{})
 }
