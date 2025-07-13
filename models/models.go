ef2d7c6b569b253f6a2cdbd25afc39b9ada4ad19
upc <upc@europeia.dev>
2025-07-12 20:13:40 -0400

    feat: dispatch, rmbpost, template models

--- models/models.go
+++ models/models.go
@@ -1,9 +1,77 @@
 package models
 
-type Telegram struct {
+import "time"
+
+type NewTelegram struct {
 	Sender    string `json:"sender"`
 	Recipient string `json:"recipient"`
 	Id        string `json:"id"`
 	Secret    string `json:"secret_key"`
 	Type      string `json:"tg_type"`
+}
+
+type DeleteTelegram struct {
+	Recipient string `json:"recipient"`
+	ID        string `json:"id"`
+}
+
+type Dispatch struct {
+	Id          int       `json:"id"`
+	Nation      string    `json:"nation"`
+	Category    int       `json:"category"`
+	SubCategory int       `json:"subcategory"`
+	Title       string    `json:"title"`
+	Text        string    `json:"text"`
+	CreatedBy   string    `json:"created_by"`
+	ModifiedAt  time.Time `json:"modified_at"`
+}
+
+type NewDispatch struct {
+	Nation      string `json:"nation"`
+	Title       string `json:"title"`
+	Text        string `json:"text"`
+	Category    int    `json:"category"`
+	SubCategory int    `json:"subcategory"`
+}
+
+type EditDispatch struct {
+	Id          int    `json:"-"`
+	Title       string `json:"title"`
+	Text        string `json:"text"`
+	Category    int    `json:"category"`
+	SubCategory int    `json:"subcategory"`
+}
+
+type DispatchStatus struct {
+	ID         int       `json:"id"`
+	Action     string    `json:"action"`
+	Status     string    `json:"status"`
+	DispatchID int       `json:"dispatch_id"`
+	Error      string    `json:"error"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+}
+
+type RmbPost struct {
+	Nation string `json:"nation"`
+	Region string `json:"region"`
+	Text   string `json:"text"`
+}
+
+type Template struct {
+	ID          string    `json:"id"`
+	Nation      string    `json:"nation"`
+	Tgid        int       `json:"tgid"`
+	Key         string    `json:"key"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	ModifiedAt  time.Time `json:"modified_at"`
+}
+
+type NewOrEditTemplate struct {
+	ID          string `json"-"`
+	Nation      string `json:"nation"`
+	Tgid        int    `json:"tgid"`
+	Key         string `json:"key"`
+	Description string `json:"description"`
 }
